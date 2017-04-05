6aef166a4c41f88b18f6cb60a880645e8c9b2c8b
Jpadilla1293 <jpadilla1293@gmail.com>
2017-04-05 12:13:41 -0400

    added inputFieldClass prop

--- dist/Calendar.js
+++ dist/Calendar.js
@@ -9,254 +9,270 @@ var Utils = require('./Utils');
 var _keyDownActions = Utils.keyDownActions;
 
 module.exports = React.createClass({displayName: "exports",
+  propTypes: {
+    closeOnSelect: React.PropTypes.bool,
+    computableFormat: React.PropTypes.string,
+    date: React.PropTypes.any,
+    format: React.PropTypes.string,
+    minView: React.PropTypes.number,
+    onBlur: React.PropTypes.func,
+    onChange: React.PropTypes.func,
+    placeholder: React.PropTypes.string,
+    hideTouchKeyboard: React.PropTypes.bool,
+    inputFieldClass: React.PropTypes.string
+  },
 
-    propTypes: {
-        closeOnSelect: React.PropTypes.bool,
-        computableFormat: React.PropTypes.string,
-        date: React.PropTypes.any,
-        format: React.PropTypes.string,
-        minView: React.PropTypes.number,
-        onBlur: React.PropTypes.func,
-        onChange: React.PropTypes.func,
-        placeholder: React.PropTypes.string,
-        hideTouchKeyboard: React.PropTypes.bool,
-    },
+  getInitialState: function() {
+    var date = this.props.date ? moment(this.props.date) : null,
+      format = this.props.format || 'MM-DD-YYYY',
+      minView = parseInt(this.props.minView, 10) || 0,
+      computableFormat = this.props.computableFormat || 'MM-DD-YYYY';
 
-    getInitialState: function() {
-        var date = this.props.date ? moment(this.props.date) : null,
-            format = this.props.format || 'MM-DD-YYYY',
-            minView = parseInt(this.props.minView, 10) || 0,
-            computableFormat = this.props.computableFormat || 'MM-DD-YYYY';
+    return {
+      date: date,
+      format: format,
+      computableFormat: computableFormat,
+      inputValue: date ? date.format(format) : null,
+      views: ['days', 'months', 'years'],
+      minView: minView,
+      currentView: minView || 0,
+      isVisible: false
+    };
+  },
 
-        return {
-            date: date,
-            format: format,
-            computableFormat: computableFormat,
-            inputValue: date ? date.format(format) : null,
-            views: ['days', 'months', 'years'],
-            minView: minView,
-            currentView: minView || 0,
-            isVisible: false
-        };
-    },
+  componentDidMount: function() {
+    document.addEventListener('click', this.documentClick);
+  },
 
-    componentDidMount: function() {
-        document.addEventListener('click', this.documentClick);
-    },
+  componentWillUnmount: function() {
+    document.removeEventListener('click', this.documentClick);
+  },
 
-    componentWillUnmount: function() {
-        document.removeEventListener('click', this.documentClick);
-    },
+  componentWillReceiveProps: function(nextProps) {
+    this.setState({
+      date: nextProps.date ? moment(nextProps.date) : this.state.date,
+      inputValue: nextProps.date
+        ? moment(nextProps.date).format(this.state.format)
+        : null
+    });
+  },
 
-    componentWillReceiveProps: function(nextProps) {
-        this.setState({
-            date: nextProps.date ? moment(nextProps.date) : this.state.date,
-            inputValue: nextProps.date ? moment(nextProps.date).format(this.state.format) : null
-        });
-    },
+  keyDown: function(e) {
+    _keyDownActions.call(this, e.keyCode);
+  },
 
-    keyDown: function (e) {
-        _keyDownActions.call(this, e.keyCode);
-    },
+  nextView: function() {
+    this.setState({
+      currentView: ++this.state.currentView
+    });
+  },
 
-    nextView: function () {
-        this.setState({
-            currentView: ++this.state.currentView
-        });
-    },
+  prevView: function(date) {
+    if (this.state.currentView === this.state.minView) {
+      this.setState({
+        date: date,
+        inputValue: date.format(this.state.format),
+        isVisible: false
+      });
 
-    prevView: function (date) {
-        if (this.state.currentView === this.state.minView) {
-            this.setState({
-                date: date,
-                inputValue: date.format(this.state.format),
-                isVisible: false
-            });
+      if (this.props.onChange) {
+        this.props.onChange(date.format(this.state.computableFormat));
+      }
+    } else {
+      this.setState({
+        date: date,
+        currentView: --this.state.currentView
+      });
+    }
+  },
 
-            if (this.props.onChange) {
-                this.props.onChange(date.format(this.state.computableFormat));
-            }
+  setDate: function(date, isDayView) {
+    this.setState({
+      date: date,
+      inputValue: date.format(this.state.format),
+      isVisible: this.props.closeOnSelect && isDayView
+        ? !this.state.isVisible
+        : this.state.isVisible
+    });
 
-        } else {
-            this.setState({
-                date: date,
-                currentView: --this.state.currentView
-            });
-        }
-    },
+    if (this.props.onChange) {
+      this.props.onChange(date.format(this.state.computableFormat));
+    }
+  },
 
-    setDate: function (date, isDayView) {
-        this.setState({
-            date: date,
-            inputValue: date.format(this.state.format),
-            isVisible: this.props.closeOnSelect && isDayView ? !this.state.isVisible : this.state.isVisible
-        });
+  changeDate: function(e) {
+    this.setState({
+      inputValue: e.target.value
+    });
+  },
 
-        if (this.props.onChange) {
-            this.props.onChange(date.format(this.state.computableFormat));
-        }
-    },
+  inputBlur: function(e) {
+    var date = this.state.inputValue,
+      newDate = null,
+      computableDate = null,
+      format = this.state.format;
 
-    changeDate: function (e) {
-        this.setState({
-            inputValue: e.target.value
-        })
-    },
+    if (date) {
+      // format, with strict parsing true, so we catch bad dates
+      newDate = moment(date, format, true);
 
-    inputBlur: function (e) {
-        var date = this.state.inputValue,
-            newDate = null,
-            computableDate = null,
-            format = this.state.format;
+      // if the new date didn't match our format, see if the native
+      // js date can parse it
+      if (!newDate.isValid()) {
+        var d = new Date(date);
 
-        if (date) {
-            // format, with strict parsing true, so we catch bad dates
-            newDate = moment(date, format, true);
-
-            // if the new date didn't match our format, see if the native
-            // js date can parse it
-            if (!newDate.isValid()) {
-                var d = new Date(date);
-
-                // if native js cannot parse, just make a new date
-                if (isNaN(d.getTime())) {
-                    d = new Date();
-                }
-
-                newDate = moment(d);
-            }
-
-            computableDate = newDate.format(this.state.computableFormat);
+        // if native js cannot parse, just make a new date
+        if (isNaN(d.getTime())) {
+          d = new Date();
         }
 
-        this.setState({
-            date: newDate,
-            inputValue: newDate ? newDate.format(format) : null
-        });
+        newDate = moment(d);
+      }
 
-        if (this.props.onChange) {
-            this.props.onChange(computableDate);
-        }
-
-        if (this.props.onBlur) {
-            this.props.onBlur(e, computableDate);
-        }
-    },
-
-    //small hack for hide calendar
-    isCalendar: false,
-
-    documentClick: function () {
-        if (!this.isCalendar) {
-            this.setVisibility(false);
-        }
-        this.isCalendar = false;
-    },
-
-    calendarClick: function () {
-        this.isCalendar = true;
-    },
-
-    todayClick: function () {
-        var today = moment();
-
-        this.setState({
-            date: today,
-            inputValue: today.format(this.state.format),
-            currentView: this.state.minView
-        });
-
-        if (this.props.onChange) {
-            this.props.onChange(today.format(this.state.computableFormat));
-        }
-    },
-
-    toggleClick: function () {
-        this.isCalendar = true;
-        this.setVisibility();
-    },
-
-    setVisibility: function (val) {
-        var value = val !== undefined ? val : !this.state.isVisible;
-        var eventMethod = value ? 'addEventListener' : 'removeEventListener';
-        document[eventMethod]('keydown', this.keyDown);
-
-        this.setState({
-            isVisible: value
-        });
-    },
-
-    render: function () {
-
-        // its ok for this.state.date to be null, but we should never
-        // pass null for the date into the calendar pop up, as we want
-        // it to just start on todays date if there is no date set
-        var calendarDate = this.state.date || moment();
-
-        var view;
-        switch (this.state.currentView) {
-            case 0:
-                view = React.createElement(DaysView, {
-                    date: calendarDate, 
-                    setDate: this.setDate, 
-                    nextView: this.nextView});
-                break;
-            case 1:
-                view = React.createElement(MonthsView, {
-                    date: calendarDate, 
-                    setDate: this.setDate, 
-                    nextView: this.nextView, 
-                    prevView: this.prevView});
-                break;
-            case 2:
-                view = React.createElement(YearsView, {
-                    date: calendarDate, 
-                    setDate: this.setDate, 
-                    prevView: this.prevView});
-                break;
-        }
-
-        var calendar = !this.state.isVisible ? '' :
-            React.createElement("div", {className: "input-calendar-wrapper", onClick: this.calendarClick}, 
-                view, React.createElement("span", {className: "today-btn", onClick: this.todayClick}, "Today")
-            );
-
-        var iconClass = cs({
-            'fa': true,
-            'fa-calendar': !this.state.isVisible,
-            'fa-calendar-o': this.state.isVisible
-        });
-
-        var readOnly = false;
-
-        if(this.props.hideTouchKeyboard) {
-            // do not break server side rendering:
-            try {
-                if(/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-                    readOnly = true;
-                }
-            } catch (e) {
-
-            }
-        }
-
-        return (
-            React.createElement("div", {className: "input-calendar"}, 
-                React.createElement("input", {type: "text", 
-                    className: "input-calendar-value", 
-                    value: this.state.inputValue, 
-                    onBlur: this.inputBlur, 
-                    onChange: this.changeDate, 
-                    onFocus: this.props.openOnInputFocus ? this.toggleClick : '', 
-                    placeholder: this.props.placeholder, 
-                    readOnly: readOnly}), 
-
-                React.createElement("span", {onClick: this.toggleClick, className: "icon-wrapper calendar-icon"}, 
-                    React.createElement("i", {className: iconClass})
-                ), 
-                calendar
-            )
-        );
+      computableDate = newDate.format(this.state.computableFormat);
     }
 
+    this.setState({
+      date: newDate,
+      inputValue: newDate ? newDate.format(format) : null
+    });
+
+    if (this.props.onChange) {
+      this.props.onChange(computableDate);
+    }
+
+    if (this.props.onBlur) {
+      this.props.onBlur(e, computableDate);
+    }
+  },
+
+  //small hack for hide calendar
+  isCalendar: false,
+
+  documentClick: function() {
+    if (!this.isCalendar) {
+      this.setVisibility(false);
+    }
+    this.isCalendar = false;
+  },
+
+  calendarClick: function() {
+    this.isCalendar = true;
+  },
+
+  todayClick: function() {
+    var today = moment();
+
+    this.setState({
+      date: today,
+      inputValue: today.format(this.state.format),
+      currentView: this.state.minView
+    });
+
+    if (this.props.onChange) {
+      this.props.onChange(today.format(this.state.computableFormat));
+    }
+  },
+
+  toggleClick: function() {
+    this.isCalendar = true;
+    this.setVisibility();
+  },
+
+  setVisibility: function(val) {
+    var value = val !== undefined ? val : !this.state.isVisible;
+    var eventMethod = value ? 'addEventListener' : 'removeEventListener';
+    document[eventMethod]('keydown', this.keyDown);
+
+    this.setState({
+      isVisible: value
+    });
+  },
+
+  render: function() {
+    // its ok for this.state.date to be null, but we should never
+    // pass null for the date into the calendar pop up, as we want
+    // it to just start on todays date if there is no date set
+    var calendarDate = this.state.date || moment();
+
+    var view;
+    switch (this.state.currentView) {
+      case 0:
+        view = (
+          React.createElement(DaysView, {
+            date: calendarDate, 
+            setDate: this.setDate, 
+            nextView: this.nextView}
+          )
+        );
+        break;
+      case 1:
+        view = (
+          React.createElement(MonthsView, {
+            date: calendarDate, 
+            setDate: this.setDate, 
+            nextView: this.nextView, 
+            prevView: this.prevView}
+          )
+        );
+        break;
+      case 2:
+        view = (
+          React.createElement(YearsView, {
+            date: calendarDate, 
+            setDate: this.setDate, 
+            prevView: this.prevView}
+          )
+        );
+        break;
+    }
+
+    var calendar = !this.state.isVisible
+      ? ''
+      : React.createElement("div", {className: "input-calendar-wrapper", onClick: this.calendarClick}, 
+          view, 
+          React.createElement("span", {className: "today-btn", onClick: this.todayClick}, "Today")
+        );
+
+    var iconClass = cs({
+      fa: true,
+      'fa-calendar': !this.state.isVisible,
+      'fa-calendar-o': this.state.isVisible
+    });
+
+    var readOnly = false;
+
+    if (this.props.hideTouchKeyboard) {
+      // do not break server side rendering:
+      try {
+        if (
+          /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+            navigator.userAgent
+          )
+        ) {
+          readOnly = true;
+        }
+      } catch (e) {}
+    }
+
+    return (
+      React.createElement("div", {className: "input-calendar"}, 
+        React.createElement("input", {
+          type: "text", 
+          className: "input-calendar-value" + " " + this.props.inputFieldClass, 
+          value: this.state.inputValue, 
+          onBlur: this.inputBlur, 
+          onChange: this.changeDate, 
+          onFocus: this.props.openOnInputFocus ? this.toggleClick : '', 
+          placeholder: this.props.placeholder, 
+          readOnly: readOnly}
+        ), 
+
+        React.createElement("span", {onClick: this.toggleClick, className: "icon-wrapper calendar-icon"}, 
+          React.createElement("i", {className: iconClass})
+        ), 
+        calendar
+      )
+    );
+  }
 });
